69ae422581032f6eb3132aa97842cab9bfc3469a
RajPradhan75 <pradhan.raj5@gmail.com>
2023-06-04 03:54:46 +0530

    Added Token generation logic

--- src/main/java/com/security/authmechanism/config/JwtService.java
+++ src/main/java/com/security/authmechanism/config/JwtService.java
@@ -1,9 +1,16 @@
 package com.security.authmechanism.config;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -11,8 +18,36 @@ public class JwtService {
 	
 	private static final String SECRET_KEY = "357538782F413F4428472B4B6250655368566D59713373367639792442264529";
 
-	public String extractUsername(String jwt) {
-		return null;
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	//generate token without extra claim
+	public String generateToken(UserDetails userDetails) {
+		return generateToken(new HashMap<>(), userDetails);
+	}
+	
+	
+	//generate token with extra claim
+	public String generateToken(
+			Map<String, Object> extraClaims,
+			UserDetails userDetails
+			) {
+		return Jwts
+				.builder()
+				.setClaims(extraClaims)
+				.setSubject(userDetails.getUsername())
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 24)) 
+				.signWith(getSignInKey(), SignatureAlgorithm.HS256)
+				.compact();
+	}
+	
+
+	
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
 	}
 	
 	private Claims extractAllClaims(String token) {
