067439d6e38fad7ef2502bfd077641e7d1b412d7
Raj8655 <42677693+Raj8655@users.noreply.github.com>
2023-06-03 22:53:41 +0530

    db configuration and USER Repo added

---
+++ src/main/java/com/security/authmechanism/AuthmechanismApplication.java
@@ -0,0 +1,13 @@
+package com.security.authmechanism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthmechanismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthmechanismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/authmechanism/AuthmechanismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.authmechanism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthmechanismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
