8d0be957c43dd3eeb424e9daba9c282b68635fe7
Raj8655 <42677693+Raj8655@users.noreply.github.com>
2023-06-04 02:40:03 +0530

    Created Role Enum and JwtAuth class

---
+++ src/main/java/com/security/authmechanism/dao/Role.java
@@ -0,0 +1,9 @@
+package com.security.authmechanism.dao;
+
+public enum Role {
+	
+	
+	
+	
+	
+}
--- src/main/java/com/security/authmechanism/entity/User.java
+++ src/main/java/com/security/authmechanism/entity/User.java
@@ -1,8 +1,18 @@
 package com.security.authmechanism.entity;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.security.authmechanism.dao.Role;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,7 +26,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
@@ -24,7 +34,43 @@ public class User {
 	@Column(unique = true)
 	private String email;
 	private String password;
-	private String role;
-	private boolean enabled;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
 
 }
